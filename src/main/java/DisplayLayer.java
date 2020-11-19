5b00550950ca5bda0c701372af5ee1c4c0477116
rohankumar28 <rohankumar0428@gmail.com>
2020-11-19 09:51:33 +0530

    Update DisplayLayer.java

--- src/main/java/DisplayLayer.java
+++ src/main/java/DisplayLayer.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 
 public class DisplayLayer {
-    public static void main(String args[]){
-        
+    public static void main(String args[]){ 
         // switch user_id
         //post "rohan kumar"
         Scanner scanner = new Scanner(System.in);
